82a4c79dca7f23c9d988a4f51dbdd57f7d4f970b
Administrator <styier@qq.com>
2016-09-27 21:58:02 +0800

    第二章分析

--- form_template/alpha_munger/main.py
+++ form_template/alpha_munger/main.py
@@ -1,6 +1,6 @@
+# coding:utf-8
 import os.path
 import random
-
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
@@ -8,7 +8,7 @@ import tornado.web
 
 from tornado.options import define, options
 
-define("port", default=8001, help="run on the given port", type=int)
+define("port", default=8000, help="run on the given port", type=int)
 
 
 class IndexHandler(tornado.web.RequestHandler):
@@ -31,7 +31,7 @@ class MungedPageHandler(tornado.web.RequestHandler):
         source_map = self.map_by_first_letter(source_text)
         change_lines = text_to_change.split('\r\n')
         self.render('munged.html', source_map=source_map, change_lines=change_lines,
-                    choice=random.choice)
+                    choice=random.choice)  # 这个函数以一个列表作为输入，返回列表中的任一元素。
 
 
 if __name__ == '__main__':
@@ -42,6 +42,11 @@ if __name__ == '__main__':
         static_path=os.path.join(os.path.dirname(__file__), "static"),
         debug=True
     )
+    '''
+    debug=True的使用。它调用了一个便利的测试模式：tornado.autoreload模块，此时，一旦主要的Python文件被修改，
+    Tornado将会尝试重启服务器，并且在模板改变时会进行刷新。对于快速改变和实时更新这非常棒，但不要再生产上使用它，
+    因为它将防止Tornado缓存模板！
+    '''
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
